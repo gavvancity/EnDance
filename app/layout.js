3d52d329ba35066706ecd5c9ebffbe133c0e1dd3
MMatyskiewicz2004 <mmatyskiewicz1@my.bcit.caconsole.log(horseName)>
2025-05-18 22:36:01 -0700

    fixed layout and removed page.js so I can redo it

--- app/layout.js
+++ app/layout.js
@@ -1,13 +1,8 @@
-import { Geist, Geist_Mono } from "next/font/google";
+import { Karla } from "next/font/google";
 import "./globals.css";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
+const karla = Karla({
+  variable: "--font-h1",
   subsets: ["latin"],
 });
 
@@ -22,11 +17,10 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
+      <body className={`${karla.variable} antialiased`} style={{ background: '#e0e0e0' }}>
         {children}
       </body>
     </html>
   );
 }
+
